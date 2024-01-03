47cc596196a72532f0c9bf75d86241eef1d0c454
gen_lin <gen_lin@goodwill.com.tw>
2024-01-03 10:11:09 +0800

    build: :package: 測試
    modify

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "scope-empty": [2,'never'],
     "type-empty": [2,"never"],
     "subject-case": [0],
+    "signed-off-by": [1,'always','Dennis'],
     "type-enum": [2,'always',[
       'feat',
       'fix',
