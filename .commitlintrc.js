c1bc32bfe15773e0ef0b7bc9be6d79271519a51e
gen_lin <gen_lin@goodwill.com.tw>
2024-01-03 09:06:10 +0800

    feat: :sparkles: test

---
+++ .commitlintrc.js
@@ -0,0 +1,69 @@
+// .commitlintrc.js
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+  },
+  prompt: {
+    alias: { fd: "docs: fix typos" },
+    messages: {
+      type: "選擇您要提交的變更類型:",
+      scope: "表示此變更的範圍（可選）:",
+      customScope: "表示此更改的範圍:",
+      subject: "寫一個關於變更的簡短、命令式的時態描述:\n",
+      body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+      breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
+      footerPrefixesSelect: "Select the ISSUES type of changeList by this change (optional):",
+      customFooterPrefix: "Input ISSUES prefix:",
+      footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
+      generatingByAI: 'Generating your AI commit subject...',
+      generatedSelectByAI: 'Select suitable subject by AI generated:',
+      confirmCommit: "Are you sure you want to proceed with the commit above?"
+    },
+    types: [
+      { value: "feat", name: "feat:     ✨  A new feature", emoji: ":sparkles:" },
+      { value: "fix", name: "fix:      🐛  A bug fix", emoji: ":bug:" },
+      { value: "docs", name: "docs:     📝  Documentation only changes", emoji: ":memo:" },
+      { value: "style", name: "style:    💄  Changes that do not affect the meaning of the code", emoji: ":lipstick:" },
+      { value: "refactor", name: "refactor: ♻️   A code change that neither fixes a bug nor adds a feature", emoji: ":recycle:" },
+      { value: "perf", name: "perf:     ⚡️  A code change that improves performance", emoji: ":zap:" },
+      { value: "test", name: "test:     ✅  Adding missing tests or correcting existing tests", emoji: ":white_check_mark:" },
+      { value: "build", name: "build:    📦️   Changes that affect the build system or external dependencies", emoji: ":package:" },
+      { value: "ci", name: "ci:       🎡  Changes to our CI configuration files and scripts", emoji: ":ferris_wheel:" },
+      { value: "chore", name: "chore:    🔨  Other changes that don't modify src or test files", emoji: ":hammer:" },
+      { value: "revert", name: "revert:   ⏪️  Reverts a previous commit", emoji: ":rewind:" }
+    ],
+    useEmoji: true,
+    emojiAlign: "center",
+    useAI: false,
+    aiNumber: 1,
+    themeColorCode: "",
+    scopes: [],
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlign: "bottom",
+    customScopesAlias: "custom",
+    emptyScopesAlias: "empty",
+    upperCaseSubject: false,
+    markBreakingChangeMode: false,
+    allowBreakingChanges: ['feat', 'fix'],
+    breaklineNumber: 100,
+    breaklineChar: "|",
+    skipQuestions: [],
+    issuePrefixes: [{ value: "closed", name: "closed:   ISSUES has been processed" }],
+    customIssuePrefixAlign: "top",
+    emptyIssuePrefixAlias: "skip",
+    customIssuePrefixAlias: "custom",
+    allowCustomIssuePrefix: true,
+    allowEmptyIssuePrefix: true,
+    confirmColorize: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: Infinity,
+    minSubjectLength: 0,
+    scopeOverrides: undefined,
+    defaultBody: "",
+    defaultIssues: "",
+    defaultScope: "",
+    defaultSubject: ""
+  }
+};
